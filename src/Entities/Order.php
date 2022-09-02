8aa66ccd42e342860c80f41b6fa90583d423b258
Bohdan <bohdan.shevchenko2@nure.ua>
2022-09-02 13:58:14 +0300

    Changed style of merchantOrderId property name

--- src/Entities/Order.php
+++ src/Entities/Order.php
@@ -50,7 +50,7 @@ final class Order implements MultiFieldsEntityInterface
         );
 
         if ($merchantOrderId) $this->merchantOrderId = $this->createSimpleField(
-            propertyName: 'merchantOrderId',
+            propertyName: 'merchant_order_id',
             value: $merchantOrderId
         );
 
